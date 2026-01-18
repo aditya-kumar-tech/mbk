0f4a20800f326bb6ee91a4d95e57b876a9bac8b2
aditya-kumar-tech <77339582+aditya-kumar-tech@users.noreply.github.com>
2026-01-18 16:23:53 +0530

    Update universal-loader.js

--- data/gs/core/universal-loader.js
+++ data/gs/core/universal-loader.js
@@ -1,15 +1,8 @@
-// ULTRA MINIMAL - सिर्फ page detect + JSON load
 (function(){
-    window.isGoldPage=!!document.querySelector('#g22kt');
-    window.isSilverPage=!!document.querySelector('#silvr_pricet');
-    
-    // Group-wise JSONs load
-    if(window.isGoldPage){
-        fetch('https://aditya-kumar-tech.github.io/mbk/data/gs/gold-groups.json')
-        .then(r=>r.json()).then(d=>window.gsConfig=d);
+    if(document.querySelector('#g22kt')){
+        fetch('https://aditya-kumar-tech.github.io/mbk/data/gs/gold-groups.json').then(r=>r.json()).then(d=>window.gsConfig=d);
     }
-    if(window.isSilverPage){
-        fetch('https://aditya-kumar-tech.github.io/mbk/data/gs/silver-groups.json')
-        .then(r=>r.json()).then(d=>window.gsConfig=d);
+    if(document.querySelector('#silvr_pricet')){
+        fetch('https://aditya-kumar-tech.github.io/mbk/data/gs/silver-groups.json').then(r=>r.json()).then(d=>window.gsConfig=d);
     }
 })();

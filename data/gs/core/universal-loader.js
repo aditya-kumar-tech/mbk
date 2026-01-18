6e18b86a238acd88caf1151bfb5cdc5ad003a5c0
aditya-kumar-tech <77339582+aditya-kumar-tech@users.noreply.github.com>
2026-01-18 16:39:09 +0530

    Update universal-loader.js

--- data/gs/core/universal-loader.js
+++ data/gs/core/universal-loader.js
@@ -1,4 +1,13 @@
+// mbk/data/gs/core/universal-loader.js
 (function(){
+    // Plotly पहले load करें
+    if(typeof Plotly==='undefined'){
+        const plotly=document.createElement('script');
+        plotly.src='https://cdn.plot.ly/plotly-latest.min.js';
+        document.head.appendChild(plotly);
+    }
+    
+    // आपके existing code
     if(document.querySelector('#g22kt')){
         fetch('https://aditya-kumar-tech.github.io/mbk/data/gs/gold-groups.json').then(r=>r.json()).then(d=>window.gsConfig=d);
     }
